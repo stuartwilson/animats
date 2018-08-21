7bd837c72ca269514e067b270c52070983ac4403
Stuart Wilson <s.p.wilson@sheffield.ac.uk>
2018-08-21 14:49:35 +0100

    moved all shapematching into header file

--- sim/template.cpp
+++ sim/template.cpp
@@ -1,7 +1,8 @@
 /*!
  * A template RD system
  */
-#include "rd_template.h"
+#include "shapeMatch.h"
+#include "sphere.h"
 
 #include "morph/display.h"
 #include <iostream>
@@ -9,6 +10,43 @@
 #include <string>
 
 using namespace std;
+
+
+
+// PLANE BOUNDARY, E.G., FOR FLOOR
+void boundary(ShapeMatch& a, double loc, int axis, double bounce, int contactFlag){
+
+    if (loc<0.){
+        for(int j=0;j<a.Npoint;j++){
+            if (a.Xtemp[j][axis]<loc+a.rad[j]){
+                vec normal = zeros<vec>(3);
+                normal(axis) = 1.0;
+                vec vNew = -2.*dot(a.Vel[j],normal)*normal+a.Vel[j];
+                a.Xnew[j]=a.Xnow[j]+vNew*a.params.dt*bounce;
+                if(a.Xnew[j][axis]<loc+a.rad[j]){
+                    a.Xnew[j][axis]=loc+a.rad[j];
+                    //a.contacts[j]=contactFlag;
+                }
+            }
+        }
+    }else{
+        for(int j=0;j<a.Npoint;j++){
+            if (a.Xtemp[j][axis]>loc-a.rad[j]){
+                vec normal = zeros<vec>(3);
+                normal(axis) = -1.0;
+                vec vNew = -2.*dot(a.Vel[j],normal)*normal+a.Vel[j];
+                a.Xnew[j]=a.Xnow[j]+vNew*a.params.dt*bounce;
+                if(a.Xnew[j][axis]>loc-a.rad[j]){
+                    a.Xnew[j][axis]=loc-a.rad[j];
+                    //a.contacts[j]=contactFlag;
+                }
+            }
+        }
+    }
+}
+
+
+
 
 int main (int argc, char **argv)
 {
@@ -26,64 +64,92 @@ int main (int argc, char **argv)
     vector<morph::Gdisplay> displays;
     vector<double> fix(3, 0.0);
     vector<double> eye(3, 0.0);
-    eye[2] = -0.4;
     vector<double> rot(3, 0.0);
 
-    double rhoInit = 1.5;
+    double rhoInit = 5.;
     string worldName(argv[1]);
-    string winTitle = worldName + ": window name";
-    displays.push_back (morph::Gdisplay (1020, 300, 100, 0, winTitle.c_str(), rhoInit, 0.0, 0.0));
+    string winTitle = worldName + ": animat arena";
+    displays.push_back (morph::Gdisplay (500, 500, 100, 0, winTitle.c_str(), rhoInit, 0.0, 0.0));
     displays.back().resetDisplay (fix, eye, rot);
     displays.back().redrawDisplay();
-
-    // SW - Contours
-    winTitle = worldName + ": contours";
-    displays.push_back (morph::Gdisplay (500, 500, 100, 900, winTitle.c_str(), rhoInit, 0.0, 0.0, displays[0].win));
-    displays.back().resetDisplay (fix, eye, rot);
-    displays.back().redrawDisplay();
-
     // Instantiate the model object
-    RD_Template RD;
+    ShapeMatch SM;
 
-    // Do any modifications to RD parameters here.
-    // RD.myparam = 3.4;
+    sphere S(7);
+    int N = S.sphereN*S.sphereN*6;
+    vector<double> mass (N, 1.0/(double)N);
+    vector<vector<int> > clusters(1);
+    clusters.resize(1);
+    clusters[0].resize(N,0);
+    for(int i=0;i<N;i++){clusters[0][i]=i;}
+    vector<int> boneIDs;
+    vector<arma::vec> X;
+    for(int i=0;i<N;i++){X.push_back(S.sphereX[i]);};
 
-    // Call the init function, which can allocate variables and run
-    // any pre-stepping computations.
     try {
-        RD.init (displays);
+        SM.params.alpha = 0.2;
+        SM.params.beta = 1.0;
+        SM.params.type = Quadratic;
+        SM.params.dt = 0.01;
+        SM.params.allowFlip = true;
+        SM.params.volumeConservation = true;
+        SM.initShape (X, mass, clusters, boneIDs, 0.05 );
+        SM.precompute();
+
     } catch (const exception& e) {
-        cerr << "Exception initialising RD object: " << e.what() << endl;
+        cerr << "Exception initialising SM object: " << e.what() << endl;
     }
+
+
 
     // Start the loop
     bool finished = false;
     while (!finished) {
         // Step the model
         try {
-            RD.step();
+            SM.externalForces(0.,0.,-10.);
+            SM.projectPositions();
+            boundary(SM, -1.5, 2, 0.5, 0);
+            SM.integrate();
+            //SM.step();
         } catch (const exception& e) {
-            cerr << "Caught exception calling RD.step(): " << e.what() << endl;
+            cerr << "Caught exception calling SM.step(): " << e.what() << endl;
             finished = true;
         }
 
         displays[0].resetDisplay (fix, eye, rot);
         try {
-            RD.plot (displays);
+            displays[0].resetDisplay (fix, eye, rot);
+            vector<double> cl(3,0.5);
+
+            for(int i=0;i<N;i++){
+            displays[0].drawSphere(SM.Xnow[i](0),SM.Xnow[i](1),SM.Xnow[i](2),SM.rad[i],cl,9);
+            }
+
+            displays[0].redrawDisplay();
+        } catch (const exception& e) {
+            cerr << "Caught exception calling SM.plot(): " << e.what() << endl;
+            finished = true;
+        }
+        /*
+        try {
+            SM.plot (displays);
             // Save some frames
-            if (RD.stepCount % 100 == 0) {
+            if (SM.stepCount % 100 == 0) {
                 //RD.saveStuff();
             }
 
         } catch (const exception& e) {
-            cerr << "Caught exception calling RD.plot(): " << e.what() << endl;
+            cerr << "Caught exception calling SM.plot(): " << e.what() << endl;
             finished = true;
         }
 
+
         // Halt after how every many iterations suits your model:
-        if (RD.stepCount > 100) {
+        if (SM.stepCount > 100) {
             finished = true;
         }
+         */
     }
 
     return 0;
